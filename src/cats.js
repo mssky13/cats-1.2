16d9aaf5d9763c56436e9577751ee1f874976a75
mssky13 <ms.sky13@mail.ru>
2023-03-01 19:57:30 +0300

    Задание Cats, исправление замечаний

---
+++ src/cats.js
@@ -0,0 +1,63 @@
+const cats = [
+    {
+        "id": 2,
+        "name": "Пончик",
+        "rate": 7,
+        "age": 3,
+        "description": "Любит покушать",
+        "favorite": false,
+        "img_link": "https://cats.petiteweb.dev/strong/images/c2.png"
+    },
+    {
+        "id": 3,
+        "name": "Свитер",
+        "rate": 8,
+        "age": 9,
+        "favorite": false,
+        "img_link": "https://cats.petiteweb.dev/strong/images/c7.png"
+    },
+    {
+        "id": 4,
+        "name": "Лаки",
+        "rate": 6,
+        "favorite": true,
+        "img_link": "https://basket-02.wb.ru/vol151/part15100/15100746/images/big/1.jpg",
+        "age": 1,
+        "description": "Любит покушать"
+    },
+    {
+        "id": 7,
+        "name": "Счастливчик",
+        "favorite": false,
+        "rate": 10,
+        "age": 10,
+        "img_link": "https://cats.petiteweb.dev/strong/images/c14.png",
+        "description": "Любит покушать"
+    },
+    {
+        "id": 8,
+        "name": "Копейка",
+        "favorite": false,
+        "rate": 6,
+        "img_link": "https://cats.petiteweb.dev/strong/images/c9.png",
+        "age": 2,
+        "description": "Маленькая катастрофа"
+    },
+    {
+        "id": 10,
+        "name": "Маруся",
+        "favorite": true,
+        "rate": 8,
+        "description": "=)",
+        "img_link": "https://cats.petiteweb.dev/strong/images/c11.png"
+    },
+    {
+        "id": 5,
+        "name": "Варежка",
+        "favorite": true,
+        "rate": 9,
+        "age": 2,
+        "description": "Сойдёт",
+        "img_link": "https://cats.petiteweb.dev/strong/images/c13.png"
+    }
+]
